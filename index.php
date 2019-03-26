a5fc2046665c8aecbbfb645afa594556efe07c15
pushdown99 <haeyuen@gmail.com>
2019-03-26 11:35:56 +0900

    make it better

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
   <script type="text/javascript">
     jQuery(document).ready(function() {
       var heatmapInstance = h337.create({
-        radius:100,
+        radius: 60,
         container: document.querySelector('.heatmap')
       });
       $.getJSON('sensors/', function(data) {
