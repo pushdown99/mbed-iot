e899384bfd0a6d648ed324a7ffc0b9d8bd93baf0
pushdown99 <haeyuen@gmail.com>
2019-03-27 11:36:41 +0900

    make it better

--- pg/index.php
+++ pg/index.php
@@ -1,5 +1,7 @@
 <?php
 
+date_default_timezone_set('Asia/Seoul');
+
 function pg_connection_string_from_database_url() {
   extract(parse_url($_ENV["DATABASE_URL"]));
   return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
