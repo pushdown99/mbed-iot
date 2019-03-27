adb6524289795175607a17d4d547f6764bed58e5
pushdown99 <haeyuen@gmail.com>
2019-03-27 11:13:55 +0900

    make it better

--- pg/index.php
+++ pg/index.php
@@ -1,6 +1,6 @@
 <?php
 
-//$conn = pg_connect(getenv("DATABASE_URL"));
+$conn = pg_connect(getenv("DATABASE_URL"));
 
 $id = "HAEYEON";
 $ts = date("Y-m-d H:M:S");
@@ -39,6 +39,6 @@ $ch30 = 0;
 $sql  = "INSERT INTO cushion VALUES ('".$id."','".$ts."'::timestamp,".$ch0.",".$ch1.",".$ch2.",".$ch3.",".$ch4.",".$ch5.",".$ch6.",".$ch7.",".$ch8.",".$ch9.",".$ch10.",".$ch11.",".$ch12.",".$ch13.",".$ch14.",".$ch15.",".$ch16.",".$ch17.",".$ch18.",".$ch19.",".$ch20.",".$ch21.",".$ch22.",".$ch23.",".$ch24.",".$ch25.",".$ch26.",".$ch27.",".$ch28.",".$ch29.",".$ch30.")";
 
 print_r($sql);
-//$result = pg_query($conn, $sql);
+$result = pg_query($conn, $sql);
 
 ?>
