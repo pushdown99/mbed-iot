8f42290c1ae1eb590ade57d95a52c8d11990f575
pushdown99 <haeyuen@gmail.com>
2019-03-28 20:35:36 +0900

    make it better

--- index.php
+++ index.php
@@ -14,21 +14,21 @@
 
     <!-- Fontfaces CSS-->
     <link href="css/font-face.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="js/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <link href="js/font-awesome-5/css/fontawesome-all.min.css" rel="stylesheet" media="all">
+    <link href="js/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
 
     <!-- Bootstrap CSS-->
-    <link href="vendor/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
+    <link href="js/bootstrap-4.1/bootstrap.min.css" rel="stylesheet" media="all">
 
     <!-- Vendor CSS-->
-    <link href="vendor/animsition/animsition.min.css" rel="stylesheet" media="all">
-    <link href="vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
-    <link href="vendor/wow/animate.css" rel="stylesheet" media="all">
-    <link href="vendor/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
-    <link href="vendor/slick/slick.css" rel="stylesheet" media="all">
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
+    <link href="js/animsition/animsition.min.css" rel="stylesheet" media="all">
+    <link href="js/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet" media="all">
+    <link href="js/wow/animate.css" rel="stylesheet" media="all">
+    <link href="js/css-hamburgers/hamburgers.min.css" rel="stylesheet" media="all">
+    <link href="js/slick/slick.css" rel="stylesheet" media="all">
+    <link href="js/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="js/perfect-scrollbar/perfect-scrollbar.css" rel="stylesheet" media="all">
 
     <!-- Main CSS-->
     <link href="css/theme.css" rel="stylesheet" media="all">
@@ -986,24 +986,24 @@
     </div>
 
     <!-- Jquery JS-->
-    <script src="vendor/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
     <!-- Bootstrap JS-->
-    <script src="vendor/bootstrap-4.1/popper.min.js"></script>
-    <script src="vendor/bootstrap-4.1/bootstrap.min.js"></script>
+    <script src="js/bootstrap-4.1/popper.min.js"></script>
+    <script src="js/bootstrap-4.1/bootstrap.min.js"></script>
     <!-- Vendor JS       -->
-    <script src="vendor/slick/slick.min.js">
+    <script src="js/slick/slick.min.js">
     </script>
-    <script src="vendor/wow/wow.min.js"></script>
-    <script src="vendor/animsition/animsition.min.js"></script>
-    <script src="vendor/bootstrap-progressbar/bootstrap-progressbar.min.js">
+    <script src="js/wow/wow.min.js"></script>
+    <script src="js/animsition/animsition.min.js"></script>
+    <script src="js/bootstrap-progressbar/bootstrap-progressbar.min.js">
     </script>
-    <script src="vendor/counter-up/jquery.waypoints.min.js"></script>
-    <script src="vendor/counter-up/jquery.counterup.min.js">
+    <script src="js/counter-up/jquery.waypoints.min.js"></script>
+    <script src="js/counter-up/jquery.counterup.min.js">
     </script>
-    <script src="vendor/circle-progress/circle-progress.min.js"></script>
-    <script src="vendor/perfect-scrollbar/perfect-scrollbar.js"></script>
-    <script src="vendor/chartjs/Chart.bundle.min.js"></script>
-    <script src="vendor/select2/select2.min.js">
+    <script src="js/circle-progress/circle-progress.min.js"></script>
+    <script src="js/perfect-scrollbar/perfect-scrollbar.js"></script>
+    <script src="js/chartjs/Chart.bundle.min.js"></script>
+    <script src="js/select2/select2.min.js">
     </script>
 
     <!-- Main JS-->
