6ed35e87bbd84aeae0a0f086874cef61eb9b09c8
pushdown99 <haeyuen@gmail.com>
2019-03-29 16:20:26 +0900

    make itr better

--- heatmap.php
+++ heatmap.php
@@ -17,12 +17,12 @@
 <body>
 <div class="container">
   <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-heatmap">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-heatmap">
   	<div class="demo">
           <div class="heatmap"></div>
 	</div>
     </div>
-    <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4">
+    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
       <div id="info"></div>
     </div>
   </div>
