bfa2998e09002431a7505ccb9d508c85fa37ca52
pushdown99 <haeyuen@gmail.com>
2019-03-29 13:22:10 +0900

    make it better

--- sensors/index.php
+++ sensors/index.php
@@ -133,9 +133,9 @@ $data["stat"]["pos"]["front"]  = (int)($v[10]+$v[12]+$v[14]+$v[16]+$v[18]+$v[20]
 $data["stat"]["pos"]["middle"] = (int)($v[5]+$v[6]+$v[7]+$v[8]+$v[11]+$v[13]+$v[15]+$v[17]+$v[19]+$v[21]+$v[22]+$v[23]+$v[24]+$v[25]);
 $data["stat"]["pos"]["rear"]   = (int)($v[0]+$v[1]+$v[2]+$v[3]+$v[4]+$v[26]+$v[27]+$v[28]+$v[29]+$v[30]);
 
-$data["stat"]["pos"]["left"]   = (int)($v[10]+$v[12]+$v[5]+$v[6]+$v[7]+$v[8]+$v[0]+$v[1]+$v[2]);
-$data["stat"]["pos"]["center"] = (int)($v[14]+$v[16]+$v[9]+$v[11]+$v[13]+$v[15]+$v[17]+$v[19]+$v[21]+$v[3]+$v[4]+$v[26]+$v[27]);
-$data["stat"]["pos"]["right"]  = (int)($v[18]+$v[20]+$v[22]+$v[23]+$v[24]+$v[25]+$v[28]+$v[29]+$v[30]);
+$data["stat"]["pos"]["left"]   = (int)($v[10]+$v[12]+$v[5]+$v[6]+$v[7]+$v[8]+$v[9]+$v[0]+$v[1]+$v[2]);
+$data["stat"]["pos"]["center"] = (int)($v[14]+$v[16]+$v[11]+$v[13]+$v[15]+$v[17]+$v[19]+$v[3]+$v[4]+$v[26]+$v[27]);
+$data["stat"]["pos"]["right"]  = (int)($v[18]+$v[20]+$$v[21]+v[22]+$v[23]+$v[24]+$v[25]+$v[28]+$v[29]+$v[30]);
 
 $data["heatmap"]["max"]   = (int)$max;
 echo json_encode($data, JSON_PRETTY_PRINT);
