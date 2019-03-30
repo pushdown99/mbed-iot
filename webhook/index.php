3b16a124a0e4d0b546c38584b0507070c7657356
pushdown99 <haeyuen@gmail.com>
2019-03-30 19:54:27 +0900

    make it better

--- webhook/index.php
+++ webhook/index.php
@@ -141,6 +141,7 @@ if(!$local) {
     if (pg_connection_status($conn) != PGSQL_CONNECTION_OK) {
         echo "Error connecting to database.";
     }
+/*
     $result = pg_query($conn, "SELECT * FROM cushion ORDER BY ts DESC LIMIT 1");
 
     if (!pg_num_rows($result)) {
@@ -148,11 +149,7 @@ if(!$local) {
     } else {
       $prev = pg_fetch_assoc($result);
     }
-
-    $conn = pg_connect(pg_connection_string_from_database_url());
-    if (pg_connection_status($conn) != PGSQL_CONNECTION_OK) {
-        echo "Error connecting to database.";
-    }
+*/
 }
     $lst = array();
 
